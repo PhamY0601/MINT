ec3bfaaec90a5b2517832346d97127464e50782c
Your Name <19522556@gm.uit.edu.vn>
2021-06-04 22:24:23 +0700

    first commit

---
+++ js/Homepage.js
@@ -0,0 +1,11 @@
+
+function imgSlider(img)
+{
+   document.querySelector(".product").src= img;
+}
+function changeCircleColor(color)
+{
+   const circle = document.querySelector(".circle");
+   circle.style.background = color;
+}
+
